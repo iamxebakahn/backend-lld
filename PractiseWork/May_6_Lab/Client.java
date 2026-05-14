10816810a631c684cb7ad15edd90c04884af075b
Zeba <zebadwight@gmail.com>
2026-05-14 12:13:04 +0530

    Java Streams classwork assignment completed

--- PractiseWork/May_6_Lab/Client.java
+++ PractiseWork/May_6_Lab/Client.java
@@ -21,6 +21,8 @@ public class Client { // Milestone A: Task 4
             System.out.println(item.getId()+" "+item.getName()+" "+item.getPrice()+" "+item.getQuantity());
         }
         Collections.sort(items) ;
+
+
         System.out.println("----------------------------------------------");
 
         System.out.println("After sorting");
@@ -101,6 +103,62 @@ public class Client { // Milestone A: Task 4
         {
             System.out.println(book.getId()+book.getName());
         }
+
+        System.out.println("Task 1:Stream Basics and Map Transformation");
+        Book b2 = new Book("B103", "Deep Work", 900, 12, "Cal Newport");
+        Book b3 = new Book("B104", "Think Like Monk", 650, 8, "Jay Shetty");
+
+        bookInventory.addItem(b2);
+        bookInventory.addItem(b3);
+        // Stream 1 : Print all IDs
+
+        bookInventory.getAllItems().stream().map(item -> item.getId()).forEach(id -> System.out.println(id));
+
+        System.out.println("----------------------------------------------");
+
+        System.out.println("Printing all names in lowercase");
+
+        List<String> lowercasenamelist= bookInventory.getAllItems().stream().map(item-> item.getName().toLowerCase()).toList();
+        System.out.println(lowercasenamelist);
+
+        // Stream 3 : Print name lengths
+
+        List<Integer> namelengthlist =  bookInventory.getAllItems().stream().map(item -> item.getName().length()).toList();
+        System.out.println(namelengthlist);
+
+        //Task 2: Filtering and Matching
+        bookInventory.getAllItems().stream().filter(book->book.getPrice()>1000).map(book -> book.getName()).forEach(bookname -> System.out.println(bookname));
+
+          //Check if ANY item has quantity = 0
+
+        boolean haszeroitem = bookInventory.getAllItems().stream().anyMatch(book -> book.getQuantity() == 0);
+        System.out.println("Item with zero quantity"+haszeroitem);
+
+        boolean nozeroitem = bookInventory.getAllItems().stream().allMatch(book -> book.getQuantity() > 0);
+        System.out.println("All items have price >0 "+nozeroitem);
+
+        boolean nonegativeitem = bookInventory.getAllItems().stream().noneMatch(book -> book.getQuantity() < 0);
+        System.out.println("No item have price <0 "+nonegativeitem);
+
+        //Task 3: Combined Stream Operations
+        List<String> booklistwithcombinedstream= bookInventory.getAllItems().stream().filter(book -> book.getPrice() >0 && book.getQuantity()>0).map(book -> book.getName()).distinct().sorted().limit(5).toList();
+        System.out.println(booklistwithcombinedstream);
+
+        // Task 4: Reduce for Aggregation
+        int totalQuantity = bookInventory.getAllItems().stream().map(book -> book.getQuantity()).reduce(0,(totalquantity,quantity) -> totalquantity + quantity);
+        System.out.println("Total Quantity"+totalQuantity);
+
+        //Find the most expensive item using reduce()
+        double mostexpensivebook = bookInventory.getAllItems().stream().map(book -> book.getPrice()).reduce(0.0,(price1,price2) -> price1>price2 ? price1:price2);
+        System.out.println("Maximum Price : " + mostexpensivebook);
+
+        Book mostexpensivebook1 = bookInventory.getAllItems().stream().reduce((book11,book22) -> book11.getPrice() >book22.getPrice() ?book11 :book22).orElse(null);
+        if(mostexpensivebook1!=null){
+            System.out.println("Most Expensive book name:"+mostexpensivebook1.getName()+"Most Expensive book price:"+mostexpensivebook1.getPrice());
+        }
+
+        String concatenatedNames = bookInventory.getAllItems().stream().map(book -> book.getName()).reduce(" ",(finalconcatenatedbook,book2) -> finalconcatenatedbook.isEmpty() ? book2 : finalconcatenatedbook +","+ book2);
+        System.out.println("All books together:"+concatenatedNames);
     }
 }
 
